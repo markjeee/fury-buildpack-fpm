2bb2c82a845688568e2f154eb979ee1a7267db12
Mark John Buenconsejo <hi@markjeee.com>
2018-06-29 21:14:44 +0800

    Adjusted fpm output to warning and errors only.

--- lib/packguy.rb
+++ lib/packguy.rb
@@ -139,7 +139,7 @@ class Packguy
     pkg_file = File.join(packager.pkg_path, deb_package_file)
     FileUtils.mkpath(File.dirname(pkg_file))
 
-    cmd = '%s --log info -f -s dir -t deb -a %s -m %s -n %s -v %s --description "%s" --url "%s" --license "%s" --vendor "%s" -p %s -d ruby %s' %
+    cmd = '%s --log warn -f -s dir -t deb -a %s -m %s -n %s -v %s --description "%s" --url "%s" --license "%s" --vendor "%s" -p %s -d ruby %s' %
           [ fpm_exec_path,
             packager.architecture,
             packager.maintainer,
@@ -168,7 +168,7 @@ class Packguy
     pkg_file = File.join(packager.pkg_path, rpm_package_file)
     FileUtils.mkpath(File.dirname(pkg_file))
 
-    cmd = '%s --log info -f -s dir -t rpm --rpm-os linux -a %s -m %s -n %s -v %s --description "%s" --url "%s" --license "%s" --vendor "%s" -p %s -d ruby %s' %
+    cmd = '%s --log warn -f -s dir -t rpm --rpm-os linux -a %s -m %s -n %s -v %s --description "%s" --url "%s" --license "%s" --vendor "%s" -p %s -d ruby %s' %
           [ fpm_exec_path,
             packager.architecture,
             packager.maintainer,
